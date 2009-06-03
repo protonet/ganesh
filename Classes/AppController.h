cfd5233551d18b532d4b5534a66a224375092569
Reza Jelveh <toyb0mb@gmx.net>
2009-06-03 17:15:37 +0200

    added menubar basics

---
+++ Classes/AppController.h
@@ -0,0 +1,19 @@
+//
+//  AppController.h
+//  protococoa
+//
+//  Created by Reza Jelveh on 6/3/09.
+//  Copyright 2009 Acculogic GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+  IBOutlet NSMenu *statusMenu;
+  NSStatusItem *statusItem;
+  NSImage *statusImage;
+  NSImage *statusHighlightImage;
+}
+
+@end
---
+++ Classes/AppController.m
@@ -0,0 +1,14 @@
+//
+//  AppController.m
+//  protococoa
+//
+//  Created by Reza Jelveh on 6/3/09.
+//  Copyright 2009 Acculogic GmbH. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+@end
