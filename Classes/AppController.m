a4ec59ed44a849ca195e48b0be8258d47936bae0
Reza Jelveh <toyb0mb@gmx.net>
2009-06-03 19:06:05 +0200

    added basic menubar thing

--- Classes/AppController.m
+++ Classes/AppController.m
@@ -11,4 +11,36 @@
 
 @implementation AppController
 
+- (void) awakeFromNib
+{
+    // create the NSStatusBar and set its length
+    statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength] retain];
+
+    // where are the bundle files?
+    NSBundle *bundle = [NSBundle mainBundle];
+
+    // allocate and load the images into the app
+    statusImage = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"icon" ofType:@"png"]];
+    statusHighlightImage = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"icon-alt" ofType:@"png"]];
+
+    // set the images in our NSStatusItem
+    [statusItem setImage:statusImage];
+    [statusItem setAlternateImage:statusHighlightImage];
+
+    // tells the nsstatusitem what menu to load
+    [statusItem setMenu:statusMenu];
+    // sets the tooltip for the item
+    [statusItem setToolTip:@"Custom Menu Item"];
+    // enable highlighting
+    [statusItem setHighlightMode:YES];
+}
+
+- (void) dealloc
+{
+    // Release the 2 images
+    [statusImage release];
+    [statusHighlightImage release];
+    [super dealloc];
+}
+
 @end
