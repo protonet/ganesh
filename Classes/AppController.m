455d96230ebaaa2044a608bd2fc44cf471e5209c
Reza Jelveh <toyb0mb@gmx.net>
2009-06-03 23:04:02 +0200

    add the n2n startup code

--- Classes/AppController.m
+++ Classes/AppController.m
@@ -11,7 +11,98 @@
 
 @implementation AppController
 - (IBAction)connect:(id)sender {
+#if 0
+    NSLog(@"test log");
 
+    if(tuntap_open(&(eee.device), tuntap_dev_name, ip_addr, netmask, device_mac, mtu) < 0)
+        return(-1);
+
+    if(local_port > 0)
+        traceEvent(TRACE_NORMAL, "Binding to local port %d", local_port);
+
+    if(edge_init_twofish( &eee, (u_int8_t *)(encrypt_key), strlen(encrypt_key) ) < 0) return(-1);
+    eee.sinfo.sock = open_socket(local_port, eee.sinfo.is_udp_socket, 0);
+    if(eee.sinfo.sock < 0) return(-1);
+
+    if( !(eee.sinfo.is_udp_socket) ) {
+        int rc = connect_socket(eee.sinfo.sock, &(eee.supernode));
+
+        if(rc == -1) {
+            traceEvent(TRACE_WARNING, "Error while connecting to supernode\n");
+            return(-1);
+        }
+    }
+
+    update_registrations(&eee);
+
+    traceEvent(TRACE_NORMAL, "");
+    traceEvent(TRACE_NORMAL, "Ready");
+
+    /* Main loop
+     *
+     * select() is used to wait for input on either the TAP fd or the UDP/TCP
+     * socket. When input is present the data is read and processed by either
+     * readFromIPSocket() or readFromTAPSocket()
+     */
+
+    while(1) {
+        int rc, max_sock = 0;
+        fd_set socket_mask;
+        struct timeval wait_time;
+        time_t nowTime;
+
+        FD_ZERO(&socket_mask);
+        FD_SET(eee.sinfo.sock, &socket_mask);
+
+        wait_time.tv_sec = SOCKET_TIMEOUT_INTERVAL_SECS; wait_time.tv_usec = 0;
+
+        rc = select(max_sock+1, &socket_mask, NULL, NULL, &wait_time);
+        nowTime=time(NULL);
+
+        if(rc > 0)
+        {
+            /* Any or all of the FDs could have input; check them all. */
+
+            if(FD_ISSET(eee.sinfo.sock, &socket_mask))
+            {
+                /* Read a cooked socket from the internet socket. Writes on the TAP
+                 * socket. */
+                readFromIPSocket(&eee);
+            }
+
+        }
+
+        update_registrations(&eee);
+
+        numPurged =  purge_expired_registrations( &(eee.known_peers) );
+        numPurged += purge_expired_registrations( &(eee.pending_peers) );
+        if ( numPurged > 0 )
+        {
+            traceEvent( TRACE_NORMAL, "Peer removed: pending=%ld, operational=%ld",
+                        peer_list_size( eee.pending_peers ), peer_list_size( eee.known_peers ) );
+        }
+
+        if ( ( nowTime - lastStatus ) > STATUS_UPDATE_INTERVAL )
+        {
+            lastStatus = nowTime;
+
+            traceEvent( TRACE_NORMAL, "STATUS: pending=%ld, operational=%ld",
+                        peer_list_size( eee.pending_peers ), peer_list_size( eee.known_peers ) );
+        }
+    } /* while */
+#endif
+}
+
+- (void) disconnect
+{
+#if 0
+    send_deregister( &eee, &(eee.supernode));
+
+    closesocket(eee.sinfo.sock);
+    tuntap_close(&(eee.device));
+
+    edge_deinit( &eee );
+#endif
 }
 
 
