ebe79b01f5258a22db2e94743c12c7daa224fc79
Reza Jelveh <reza.jelveh@tuhh.de>
2009-10-23 23:23:43 +0200

    added prefscontroller to source

---
+++ Classes/PrefsController.h
@@ -0,0 +1,17 @@
+//
+//  PrefsController.h
+//  protococoa
+//
+//  Created by Reza Jelveh on 6/3/09.
+//  Copyright 2009 Protonet.info. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PrefsController : NSObject {
+}
+
+- (IBAction) showWindow:(id)sender;
+
+@end
---
+++ Classes/PrefsController.m
@@ -0,0 +1,15 @@
+//
+//  PrefsController.m
+//  protococoa
+//
+//  Created by Reza Jelveh on 6/3/09.
+//  Copyright 2009 Protonet.info. All rights reserved.
+//
+
+#import "PrefsController.h"
+
+
+@implementation PrefsController
+
+
+@end
