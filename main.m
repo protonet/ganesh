20ab5e6aab31017dc0f92d2822911a8912da080d
Ali Jelveh <jelveh@web.de>
2009-12-07 17:49:18 +0100

    first commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  socket
+//
+//  Created by jelveh on 07.12.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
