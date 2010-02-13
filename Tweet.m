641e7ad0992bf8a63dec9a21928820c053958ffd
Reza Jelveh <toyb0mb@gmx.net>
2010-02-13 17:15:39 +0100

    stuff

--- Tweet.m
+++ Tweet.m
@@ -15,21 +15,22 @@
 
 - (id)initWithData:(NSMutableDictionary *)data
 {
-    [super init];
-	
-//    NSLog(@"Tweet init");
-//	[self setMessage:[data objectForKey:@"message"]];
 	message = [data objectForKey:@"message"];
 
-	if (!message) {
-		return nil;
-	}
-	else
-	{
+    if (message && self = [super init]) {
 		NSString * profileUrlString = [NSString stringWithFormat:@"http://localhost:3000%@", [data objectForKey:@"user_icon_url"]];
 		NSURL *profileUrl = [NSURL URLWithString:profileUrlString];
 		userImage = [[[NSImage alloc] init] initWithContentsOfURL:profileUrl];
-		return self;
-	}	
+        return self;
+    }
+    else{
+        return nil;
+    }
+}
+
+- (void)dealloc
+{
+    [userImage release];
+    [super dealloc];
 }
 @end
