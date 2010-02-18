729178e11f2911eab78bf3e9d4abacaaba24517b
Reza Jelveh <toyb0mb@gmx.net>
2010-02-18 16:50:45 +0100

    add some formatters to the preference window

---
+++ Classes/IPFormatter.h
@@ -0,0 +1,16 @@
+//
+//  IPFormatter.h
+//  ganesh
+//
+//  Created by Reza Jelveh on 18.02.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface IPFormatter : NSFormatter {
+
+}
+
+@end
