f7656130c2296334b24b1eb576a75a15e59fb2ce
Reza Jelveh <toyb0mb@gmx.net>
2010-03-08 03:10:23 +0100

    add missing preference window files

---
+++ Classes/Preferences/PreferenceWindow.h
@@ -0,0 +1,16 @@
+//
+//  PreferenceWindow.h
+//  ganesh
+//
+//  Created by Reza Jelveh on 06.03.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PreferenceWindow : NSWindow {
+
+}
+
+@end
---
+++ Classes/Preferences/PreferenceWindow.m
@@ -0,0 +1,23 @@
+//
+//  PreferenceWindow.m
+//  ganesh
+//
+//  Created by Reza Jelveh on 06.03.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "PreferenceWindow.h"
+
+
+@implementation PreferenceWindow
+
+- (BOOL)performKeyEquivalent:(NSEvent *)event {
+    if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+        if ([[event charactersIgnoringModifiers] isEqualToString:@"w"]) {
+            [self performClose:self];
+            return YES;
+        }
+    }
+    return [super performKeyEquivalent:event];
+}
+@end
