2596b2f9fa296986e10543c224abc8223539b436
Reza Jelveh <toyb0mb@gmx.net>
2010-03-25 18:52:57 +0100

    add channel model should contain title and id which we get from the application

---
+++ Classes/Channels/Channel.h
@@ -0,0 +1,16 @@
+//
+//  Channel.h
+//  ganesh
+//
+//  Created by Reza Jelveh on 25.03.10.
+//  Copyright 2010 Protonet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Channel : NSObject {
+
+}
+
+@end
---
+++ Classes/Channels/Channel.m
@@ -0,0 +1,14 @@
+//
+//  Channel.m
+//  ganesh
+//
+//  Created by Reza Jelveh on 25.03.10.
+//  Copyright 2010 Protonet. All rights reserved.
+//
+
+#import "Channel.h"
+
+
+@implementation Channel
+
+@end
